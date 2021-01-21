57959fee338fc7f3c2455b91269dc87503d51e23
EstebanNicolucci <enicolucci4@gmail.com>
2021-01-21 15:08:16 -0300

    proyecto principal

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//scripts personalizados
+
+ $(document).ready(function() {
+            // Owl Carrousel Slider
+            $("#banner-section .owl-carousel").owlCarousel({
+                dots: true,
+                nav: true,
+                items: 1,
+                loop: true,
+                autoplay: true
+            });
+        });
+
+
+
